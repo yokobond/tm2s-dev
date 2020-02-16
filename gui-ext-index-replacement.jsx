34a00f6c2a9be2de94953f78716cc812601868fd
yokobond <koji.yokokawa@gmail.com>
2020-02-16 20:41:40 +0900

    Add setup script for tm2scratch.

---
+++ gui-ext-index-replacement.jsx
@@ -0,0 +1,12 @@
+import tm2sIconURL from './tm2scratch/tm2scratch.png';
+import tm2sInsetIconURL from './tm2scratch/tm2scratch-small.png';
+
+export default [
+    {
+        name: 'TM2Scratch',
+        extensionId: 'tm2scratch',
+        iconURL: tm2sIconURL,
+        insetIconURL: tm2sInsetIconURL,
+        description: 'Play with Teachable Machine',
+        featured: true
+    },
