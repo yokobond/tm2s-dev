ce74c0ec07927909d6b09946a490208225d739fd
yokobond <koji.yokokawa@gmail.com>
2020-11-05 17:05:37 +0900

    Improve eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,33 @@
 module.exports = {
+    "extends": [
+        "eslint:recommended"
+    ],
     "env": {
-        "browser": true,
         "es6": true,
         "node": true
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
     "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        "sourceType": "module",
+        "ecmaVersion": 2017,
     },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
+    "globals":{
+    },
+    "rules":{
+        'arrow-body-style': [2, 'as-needed'],
+        'arrow-parens': [2, 'as-needed'],
+        'arrow-spacing': [2, {
+            before: true,
+            after: true
+        }],
+        'indent': [2, 4, {"SwitchCase": 1}],
+        'no-confusing-arrow': [2],
+        'no-useless-computed-key': [2],
+        'no-useless-constructor': [2],
+        'no-var': [2],
+        'prefer-arrow-callback': [2],
+        'prefer-const': [2, {destructuring: 'all'}],
+        'prefer-template': [2],
+        'rest-spread-spacing': [2, 'never'],
+        'template-curly-spacing': [2, 'never']
     }
 };
